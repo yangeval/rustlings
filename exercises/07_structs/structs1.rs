9b993312da12116db2fcc0fa18be9ee3b4022ced
yangeval <252970318+yangeval@users.noreply.github.com>
2026-02-20 22:54:03 +0900

    07_structs

--- exercises/07_structs/structs1.rs
+++ exercises/07_structs/structs1.rs
@@ -1,10 +1,15 @@
 struct ColorRegularStruct {
     // TODO: Add the fields that the test `regular_structs` expects.
     // What types should the fields have? What are the minimum and maximum values for RGB colors?
+    red:u32,
+    green:u32,
+    blue:u32
 }
 
-struct ColorTupleStruct(/* TODO: Add the fields that the test `tuple_structs` expects */);
+//struct ColorTupleStruct(/* TODO: Add the fields that the test `tuple_structs` expects */);
+struct ColorTupleStruct(u32, u32, u32);
 
+//이건 뭔가! 유닛구조체?
 #[derive(Debug)]
 struct UnitStruct;
 
@@ -20,6 +25,11 @@ mod tests {
     fn regular_structs() {
         // TODO: Instantiate a regular struct.
         // let green =
+        let green = ColorRegularStruct {
+            red: 0,
+            green: 255,
+            blue: 0
+        };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -30,7 +40,7 @@ mod tests {
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct.
         // let green =
-
+        let green = ColorTupleStruct (0, 255, 0);
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
         assert_eq!(green.2, 0);
@@ -39,7 +49,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct.
-        // let unit_struct =
+        let unit_struct = UnitStruct;
         let message = format!("{unit_struct:?}s are fun!");
 
         assert_eq!(message, "UnitStructs are fun!");
