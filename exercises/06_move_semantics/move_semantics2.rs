17e21ecbe7eb390d77dc006d31bfc7ad9082dd89
yangeval <252970318+yangeval@users.noreply.github.com>
2026-02-20 16:32:24 +0900

    log: Ownership & Move Semantics

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,23 +1,11 @@
-fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
-    //let mut vec = vec.clone();
-    //vec.push(88);
-    
-    //# 1
-    //test에서 vec0 을 clone() 해서 복사본을 인자로 전달
-    //매개변수에서 mut로 받아서 추가해서 반환
-    /*
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+
+    let mut vec = vec;
+
     vec.push(88);
     vec
-    */
-
-    //# 2
-    // 빌려서 처리. 매개변수에 & 추가
-    let mut new_vec = vec.clone(); //빌려온 걸 새 변수에 복사해서 할당
-    new_vec.push(88);
-    new_vec
-
 }
- 
+
 fn main() {
     // You can optionally experiment here.
 }
@@ -32,11 +20,25 @@ mod tests {
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        //let vec1 = fill_vec(vec0);
-        //let vec1 = fill_vec(vec0.clone()); //1번
-        let vec1 = fill_vec(&vec0);
+        let temp_vec = vec0.clone();
+
+        let vec1 = fill_vec(temp_vec);
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
     }
 }
+
+/*
+# 문제 이해.
+- vec0이 함수를 통과해 vec1이 된다. 이때 벡터에 88이 추가된다.
+- 동시에 vec0은 기존값을 그대로 유지해야 한다. 
+
+# 어떻게 해결할 것인가?
+- 문제는 vec0의 인자로 사용되면서 소유권이 넘어가는 게 문제
+
+# 1. 일단 빌리고, 이를 복사해서 사용하기
+# 2. 빌려 줄 때 애초에 복사해서 인자로 보내기
+# 3. 빌려 주기 전에 따로 복사하고, 그걸 인자로 보내기
+
+*/
