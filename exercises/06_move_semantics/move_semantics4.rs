7743d36c55f1778f4550d743bf0d11198df8b0ca
yangeval <252970318+yangeval@users.noreply.github.com>
2026-02-20 18:25:01 +0900

    learn sec06

--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -9,10 +9,24 @@ mod tests {
     #[test]
     fn move_semantics4() {
         let mut x = Vec::new();
+
         let y = &mut x;
-        let z = &mut x;
         y.push(42);
+
+        let z = &mut x;
         z.push(13);
+
         assert_eq!(x, [42, 13]);
     }
 }
+
+/* 
+# 문제
+- 대여 규칙: 가변참조자의 제약
+- 소유권을 넘긴 게 아님. 
+- 가변 참조자(&mut)가 있으면, 그 시점에 한 명만 값을 보거나 수정할 수 있다. 
+- 비유: 동시에 한 개의 물건을 고치려는 것.
+
+# 해결
+- 동시에 x에 접근하지 않게 순서를 변경
+*/
--- exercises/06_move_semantics/move_semantics5.rs
+++ exercises/06_move_semantics/move_semantics5.rs
@@ -2,14 +2,20 @@
 
 // TODO: Fix the compiler errors without changing anything except adding or
 // removing references (the character `&`).
+// & 기호를 넣거나 빼서 컴파일 에러를 고쳐
 
 // Shouldn't take ownership
-fn get_char(data: String) -> char {
-    data.chars().last().unwrap()
+//fn get_char(data: &String) -> char {
+fn get_char(data: &String){
+    //&를 사용하지 않으면, 소유권을 가져온다. 
+    //-main의 이후 함수에서 data를 사용할 수 없게 된다. 
+    //data.chars().last().unwrap()
+    let last_char = data.chars().last().unwrap();
+    println!("{}", last_char);
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
+fn string_uppercase(mut data: String) {
     data = data.to_uppercase();
 
     println!("{data}");
@@ -18,7 +24,7 @@ fn string_uppercase(mut data: &String) {
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(data);
 }
