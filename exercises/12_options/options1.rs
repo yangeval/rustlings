f4c5a6217de7d3766e424aa336bb4d2cffe6236e
yangeval <252970318+yangeval@users.noreply.github.com>
2026-02-27 00:48:09 +0900

    feat:how learn method like duolingo

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -2,6 +2,12 @@
 // If it's before 22:00 (24-hour system), then 5 scoops are left. At 22:00,
 // someone eats it all, so no ice cream is left (value 0). Return `None` if
 // `hour_of_day` is higher than 23.
+//
+// 이 함수는 냉장고에 아이스크림이 얼마나 남았는지를 반환합니다.
+// 22:00 전(24시간제 기준)이라면 5스쿱이 남아있습니다. 22:00가 되면,
+// 누군가 모두 먹어치워 버려서 아이스크림이 전혀 남지 않게 됩니다(값 0).
+// `hour_of_day`가 23보다 크면 `None`을 반환하세요.
+
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
 }
